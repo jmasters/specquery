313b9aac8bd3a52b9a4bd9658067225574166a4b
Joe Masters <jmasters@nrao.edu>
2013-07-05 17:52:56 -0400

    Update index with new DEC info and hms coordinates.

--- static/make_csv.py
+++ static/make_csv.py
@@ -7,13 +7,46 @@ import os.path
 import glob
 import csv
 
+# from http://www.bdnyc.org/2012/10/15/decimal-deg-to-hms/
+# convert decimal degrees to HMS format
+def deg2hms(ra='', dec='', doRound=False):
+    RA, DEC, rs, ds = '', '', '', ''
+    if dec:
+        if str(dec)[0] == '-':
+            ds, dec = '-', abs(dec)
+        else:
+            ds, dec = '+', abs(dec)
+    deg = int(dec)
+    decM = abs(int((dec-deg)*60))
+    if doRound:
+        decS = int((abs((dec-deg)*60)-decM)*60)
+    else:
+        decS = (abs((dec-deg)*60)-decM)*60
+    DEC = '{0}{1:02d}:{2:02d}:{3:02.1f}'.format(ds, deg, decM, decS)
+  
+    if ra:
+        if str(ra)[0] == '-':
+            rs, ra = '-', abs(ra)
+        raH = int(ra/15)
+        raM = int(((ra/15)-raH)*60)
+        if doRound:
+            raS = int(((((ra/15)-raH)*60)-raM)*60)
+        else:
+            raS = ((((ra/15)-raH)*60)-raM)*60
+        RA = '{0}{1:02d}:{2:02d}:{3:02.1f}'.format(rs, raH, raM, raS)
+  
+    if ra and dec:
+        return (RA, DEC)
+    else:
+        return RA or DEC
+
 class CSV:
     def __init__(self,newcsv=False):
         if newcsv:
             csvfile =  open('spectral_products.csv', 'w')
             self.csvwriter = csv.writer(csvfile)
             self.csvwriter.writerow(('target', 'projid', 'date', 'ra',
-                                     'observer', 'fits', 'png'))
+                                     'dec', 'observer', 'fits', 'png'))
         else:
             csvfile =  open('spectral_products.csv', 'a')
             self.csvwriter = csv.writer(csvfile)
@@ -31,17 +64,18 @@ class CSV:
         observer = table[0]['OBSERVER'].strip()
         target = table[0]['OBJECT'].strip()
         projid = table[0]['PROJID'].strip()
-        coordtype = table[0]['CTYPE2'].strip()
-        if 'RA' == coordtype:
-            ra = str(table[0]['CRVAL2'])[:6]
+        coordtype1 = table[0]['CTYPE2'].strip()
+        coordtype2 = table[0]['CTYPE3'].strip()
+        if 'RA' == coordtype1 and 'DEC' == coordtype2:
+            ra, dec = deg2hms(ra=table[0]['CRVAL2'],dec=table[0]['CRVAL3'], doRound=True)
         else:
-            print 'ERROR: CTYPE2 not RA, it\'s', coordtype
+            print 'ERROR: CTYPE2,CTYPE3 not RA,DEC, it\'s', coordtype1, coordtype2
             sys.exit()
         
-        self.csvwriter.writerow((target, projid, date, ra, observer, fname, pngfile))
+        self.csvwriter.writerow((target, projid, date, ra, dec, observer, fname, pngfile))
 
 if __name__ == '__main__':
 
-    csv_object = CSV(newcsv=False)
+    csv_object = CSV(newcsv=True)
     fitsfiles = glob.iglob('*.fits')
     map(csv_object.make_entry, fitsfiles )
